225a86de8b5ab8c87ece4c3d4d78f84250889a85
aakashDeveloper <ahanda205@gmail.com>
2020-04-02 03:27:02 +0100

    dg

---
+++ first.js
@@ -0,0 +1,84 @@
+/////DataType in JS//////
+String "jnfgjn", 'fhrhrh' '353' 'true' "325"
+Number 8835 82583748967 908.46 .9090
+Boolean true/false 
+
+Es5
+Es6
+Ecma Script 5
+Ecma Script 6
+ExNext
+
+
+
+////Es5////
+var a = 8878
+var b = "sdgg"
+var c = true
+
+var a = 8878
+var b = "sdgg"
+var c = true
+undefined
+typeof(a)
+"number"
+typeof(b)
+"string"
+typeof(c)
+"boolean"
+var d = "78678"
+undefined
+typeof(d)
+"string"
+var d = 78678898.878787
+undefined
+typeof(d)
+"number"
+
+/////////Operations/////////
+var a = 10
+var b = 5
+a+b
+15
+a-b
+5
+a/b
+2
+a*b
+50
+
+a%b
+10%6
+4
+10%7
+3
+10%2
+0
+
+4%2
+0
+2%3
+2
+99%100
+99
+
+
+
+var a = 10
+undefined
+var 1 = "hh"
+VM1134:1 Uncaught SyntaxError: Unexpected number
+var a_b = 10
+undefined
+var a-b = 10
+VM1208:1 Uncaught SyntaxError: Unexpected token '-'
+var A = 10
+undefined
+A
+10
+var true = 1
+VM1260:1 Uncaught SyntaxError: Unexpected token 'true'
+var $ = 10
+undefined
+$
+10
---
+++ second.js
@@ -0,0 +1,114 @@
+var a= "Hi "
+var b="JavaScript"
+
+var a= "Hi "
+var b="JavaScript"
+undefined
+a+b
+"Hi JavaScript"
+a-b
+NaN
+a*b
+NaN
+a/b
+NaN
+a%b
+NaN => Not No Number
+
+var a = "hi "
+undefined
+var b = 1
+undefined
+a+b
+"hi 1"
+b+a
+"1hi "
+a+true
+"hi true"
+
+string+string = string
+string+number = string
+number+string = string
+number+number = number
+
+10+10+"10"
+"10"+10+10
+10+"10"+10
+
+10+10+"10"
+"2010"
+"10"+10+10
+"101010"
+10+"10"+10
+"101010"
+
+10+10+"10"-1
+2009
+"10"+10+10-1
+101009
+10+"10"+10-1
+101009
+
+'a'-'1'
+
+'a'-'1'
+NaN
+'1'-'1'
+0
+'1'+'1'
+"11"
+'20'*'22'
+440
+'20'*'a'
+NaN
+'20'/'5'
+4
+'a'/'5'
+NaN
+'1a'/'5'
+NaN
+'10.5'/'5'
+2.1
+
+true = 1
+false = 0
+
+var a = 10
+undefined
+var b = 'true'
+undefined
+var c = true
+
+a+b+c
+a+c+b
+b+a+c
+
+var a = 10
+undefined
+var b = 'true'
+undefined
+var c = true
+undefined
+a+b+c
+"10truetrue"
+a+c+b
+"11true"
+b+a+c
+"true10true"
+
+true+10
+11
+true+true
+2
+'true'+10
+"true10"
+true+10
+11
+'true'+true
+"truetrue"
+
+>  String add with string always give string
+> number in quotes when add with again qoutes it will give string
+    else its behave number
+> Boolean in quotes is string in all cases
+> Boolean in all operation return number
