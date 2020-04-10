4795e4c1ac68a2823401b2cf705be9530168f72d
aakashDeveloper <ahanda205@gmail.com>
2020-04-10 04:43:36 +0100

    A

---
+++ eleven.js
@@ -0,0 +1,22 @@
+localStorage.setItem('Token',"84hg8773782g5iufg89e")
+undefined
+localStorage.getItem('Token')
+"84hg8773782g5iufg89e"
+localStorage.removeItem('Token')
+undefined
+localStorage.getItem('Token')
+null
+localStorage.setItem('Token',['sfs','ds','dfdg','dggdg'])
+undefined
+localStorage.getItem('Token')
+"sfs,ds,dfdg,dggdg"
+
+
+/*
+Luck Game
+Generate  one number between 1 to 20
+Take on input from user
+if both match than user win else loose
+give 5 chance to user and if 3 or time win than win 
+else loose
+*/
--- ten.js
+++ ten.js
@@ -38,3 +38,117 @@ city.pop()
 city.pop(3)
 "Mumbai"
 
+sayHi.firstnamee
+"John"
+sayHi.greet()
+"Hi To JavaScript"
+
+
+var city = ["London", "NewYork", "Delhi", "Helsinki", "Mumbai", "Boston"]
+var city = ["London", "NewYork", "Delhi", "Helsinki", "Mumbai", "Boston"]
+undefined
+city.slice(1)
+(5) ["NewYork", "Delhi", "Helsinki", "Mumbai", "Boston"]
+city
+(6) ["London", "NewYork", "Delhi", "Helsinki", "Mumbai", "Boston"]
+city.slice(2,5)
+(3) ["Delhi", "Helsinki", "Mumbai"]
+city.splice(2,0,'Paris','Boston')
+[]
+city
+(8) ["London", "NewYork", "Paris", "Boston", "Delhi", "Helsinki", "Mumbai", "Boston"]
+city.splice(2,1)
+["Paris"]
+city
+(7) ["London", "NewYork", "Boston", "Delhi", "Helsinki", "Mumbai", "Boston"]
+city.splice(3,1,'Nice','InnsBurg')
+["Delhi"]
+city
+(8) ["London", "NewYork", "Boston", "Nice", "InnsBurg", "Helsinki", "Mumbai", "Boston"]
+var city = ["London", "NewYork", "Boston", "Nice", "InnsBurg", "Helsinki", "Mumbai", "Boston"]
+undefined
+city.indexOf('London')
+0
+city.indexOf('Mumbai')
+6
+city.indexOf('Delhi')
+-1
+city.indexOf('Pune')
+-1
+
+var a = "London,NewYork,Boston,Nice,InnsBurg"
+a.split(',')
+
+
+var url="https://us-east-2.console.aws.amazon.com/rds/home/region/us-east-2"
+url.split('/')
+
+var name="JavaScript"
+name.split("")
+(10) ["J", "a", "v", "a", "S", "c", "r", "i", "p", "t"]
+
+var a = [1,2,3]
+var b = ['a','b','c']
+var a = [1,2,3]
+var b = ['a','b','c']
+undefined
+a+b
+"1,2,3a,b,c"
+a.concat(b)
+(6) [1, 2, 3, "a", "b", "c"]
+b.concat(a)
+(6) ["a", "b", "c", 1, 2, 3]
+var c = ['d','d','d']
+undefined
+a.concat(b,c)
+(9) [1, 2, 3, "a", "b", "c", "d", "d", "d"]
+
+var city = ["London", "NewYork", "Boston", "Nice", "InnsBurg", "Helsinki", "Mumbai", "Boston"]
+
+undefined
+city.sort()
+(8) ["Boston", "Boston", "Helsinki", "InnsBurg", "London", "Mumbai", "NewYork", "Nice"]
+city.reverse()
+(8) ["Nice", "NewYork", "Mumbai", "London", "InnsBurg", "Helsinki", "Boston", "Boston"]
+city.sort().reverse()
+(8) ["Nice", "NewYork", "Mumbai", "London", "InnsBurg", "Helsinki", "Boston", "Boston"]
+var city = ["London", "NewYork", "Boston", "Nice", "InnsBurg", "Helsinki", "Mumbai", "Boston"]
+undefined
+city.includes('Delhi')
+false
+city.includes('Mumbai')
+true
+
+
+function checkval(val,index,array){
+    return val>2
+}
+
+console.log([2,6,4,7,9].some(checkval))
+VM1152:5 true
+
+undefined
+function checkval(val,index,array){
+    return val>2
+}
+
+console.log([0,1,1].some(checkval))
+VM1162:5 false
+
+var a = ['a','b','c',[1,2,3,['aa','bb'],'d'],'ff']
+undefined
+a[3]
+(5) [1, 2, 3, Array(2), "d"]
+a[3][3]
+(2) ["aa", "bb"]
+a[3][3][0]
+"aa"
+
+var a = ['a','b','c',[1,2,3,['aa','bb'],'d'],'ff']
+undefined
+a.flat()
+(9) ["a", "b", "c", 1, 2, 3, Array(2), "d", "ff"]
+a.flat(22)
+(10) ["a", "b", "c", 1, 2, 3, "aa", "bb", "d", "ff"]
+a.flat(2)
+(10) ["a", "b", "c", 1, 2, 3, "aa", "bb", "d", "ff"]
