55cb24662488f4312a85e97da4d1755a97e0c343
aakashDeveloper <ahanda205@gmail.com>
2020-04-27 02:59:12 +0100

    a

--- currencyConverter/currency.js
+++ currencyConverter/currency.js
@@ -1,4 +1,21 @@
-var url = "https://api.exchangeratesapi.io/latest?base="
+var url = "https://api.exchangeratesapi.io/latest?base=";
+
+async function convert(){
+    var base = document.getElementById('base').value;
+    var output = document.getElementById('output').value;
+    var amount = document.getElementById('amount').value;
+    var final;
+    let response = await fetch(`${url}${base}`, {method:'GET'})
+    let data = await response.json();
+    final = data.rates
+    final = `${output} ${final[output] * parseFloat(amount)}`
+    var result =`Convert value of ${base} ${amount} is ${final}`
+    document.getElementById('converted').innerText=result;
+}
+
+
+
+/*
 function convert(){
     var base = document.getElementById('base').value;
     var output = document.getElementById('output').value;
@@ -14,3 +31,4 @@ function convert(){
         document.getElementById('converted').innerText=result;
     })
 }
+*/
---
+++ jquery/first.js
@@ -0,0 +1,4 @@
+$('.scroll')[0].innerText="Tech"
+"Tech"
+$('h1')[0].innerText
+"I'm AAKASH HANDA"
